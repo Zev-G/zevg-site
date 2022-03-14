00e3a80945f5c2cbbbbce94464b5c6ee6c0d9b18
Zev <vezvez1234@gmail.com>
2022-03-13 20:55:28 -0700

    Wrote all entries for Capstone Site!!

--- src/components/capstone/projects/landing-page/RoutingEntry.svelte
+++ src/components/capstone/projects/landing-page/RoutingEntry.svelte
@@ -52,7 +52,6 @@ export default class ProjectElement {
 }`
     } />
     <p>This way each ProjectElement could contain a list of its own ProjectElements.</p>
-    <!-- <p>Restructuring the timeline was very difficult because the entirety of the code wasn't created for the timeline variable to be reassigned (something which supporting switching between multiple timelines requires). The details of refactoring the code to support this are too complicated and extensive to be documented here, but the change can be seen in <a href="https://github.com/Zev-G/zevg-site/commit/9010ca3050041589d41a586cb26f56821dcc9e42">this commit</a>.</p> -->
 </div>
 
 <style>
