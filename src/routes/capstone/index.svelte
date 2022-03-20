a6d28010bb142fd079257f4be81f16e0661b882c
Zev <vezvez1234@gmail.com>
2022-03-19 19:24:22 -0700

    Can goto main timeline entries

--- src/routes/capstone/index.svelte
+++ src/routes/capstone/index.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { goto } from "$app/navigation";
+
     import QNAs from "../../components/QNAs.svelte";
     import Timeline from "../../components/Timeline.svelte";
     import BigQuestion from "../../components/BigQuestion.svelte";
@@ -7,6 +9,12 @@
     import CapstoneQuestion from "../../components/capstone/CapstoneQuestion.svelte";
 
     import { timeline, qnas } from "../../components/capstone/data";
+
+    function navigateToPoint(point) {
+        if (point.points.length > 0) {
+            goto(`/capstone/timeline/${timeline.points.indexOf(point)}`);
+        }
+    }
 </script>
 
 <svelte:head>
@@ -19,7 +27,7 @@
             <h1 class="page-title">My Capstone</h1>
             <h2 class="subtitle">Zev Godfrey</h2>
         </div>
-        <Timeline timeline={timeline} />
+        <Timeline timeline={timeline} on:goToEntry={(event) => navigateToPoint(event.detail.point)} />
         <BigQuestion answerComponent={CapstoneQuestion} question="Capstone Question" />
         <SeeMore href="/capstone/proposal">Read more... ></SeeMore>
         <QNAs qnas={qnas} />
