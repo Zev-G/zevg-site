723dd480cb95a4d034c0fed91bc96356438dfd24
Zev-G <godfreyzev@gmail.com>
2023-05-29 21:59:48 -0700

    zink privacy policy

---
+++ src/routes/zink/privacy.svelte
@@ -0,0 +1,30 @@
+
+<div>
+    <h1 style="text-align: center;">Privacy Policy</h1>
+    <h1>Personal information</h1>
+<p>Our application does not collect or transmit any user's personally identifiable information.
+    No personal information is used, stored, secured or disclosed by the application.</p>
+<h1>    Technical information</h1>
+<p>    Limited technical information is sent (such as IP addresses included in the HTTPS calls the application makes) but none of that is used or stored.
+</p>
+<h1>Personal files</h1>
+<p>
+    Our application may access users files if and only if the user opens that specific file or an encapsulating folder with the application. Our application may maintain access to these folders or files over time, but the user maintains the ability to remove access to any file or folder at any time. The app does not collect or transmit any personal information within these files or folders.
+</p>
+<h1>
+    Changes to Privacy Policy
+</h1>
+<p>
+    We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. These changes are effective immediately after they are posted on this page.
+Report violations or get more information
+If you would like to report any violations of this policy or get more information on any app, please contact us at godfreyzev@gmail.com
+</p>
+<p>Last Updated May 29th 2023</p>
+
+</div>
+
+<style>
+    div {
+        margin: 2%;
+    }
+</style>
